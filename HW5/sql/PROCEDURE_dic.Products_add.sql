c122cefa54e53bb4052f00ce2934e8a9ee1be4b1
sbekhterev <129203989+sbekhterev@users.noreply.github.com>
2026-01-15 19:18:24 +0300

    4 day

--- HW5/sql/PROCEDURE_dic.Products_add.sql
+++ HW5/sql/PROCEDURE_dic.Products_add.sql
@@ -8,13 +8,14 @@ DECLARE
 	product_ean text;
 	product_categories text[];
 	product_price decimal(10,2);
-	price_start_date date;
-	price_end_date date;
+--	price_start_date date;
+--	price_end_date date;
+	Price_List integer;
 	unit_id integer;
 	return_product_id integer;
 	product_category text;
 	product_category_id integer;
-	required_keys text[] := ARRAY['Name', 'Unit', 'EAN', 'Category', 'Price', 'PriceStart', 'PriceEnd'];
+	required_keys text[] := ARRAY['Name', 'Unit', 'EAN', 'Category', 'Price'];
 	required_key text;
 	missing_keys text[];
 BEGIN
@@ -45,8 +46,9 @@ LOOP
 	product_unit := product_record->>'Unit';
 	product_ean := product_record->>'EAN';
 	product_price := (product_record->>'Price')::decimal(10,2);
-	price_start_date := (product_record->>'PriceStart')::date;
-	price_end_date := (product_record->>'PriceEnd')::date;
+--	price_start_date := (product_record->>'PriceStart')::date;
+--	price_end_date := (product_record->>'PriceEnd')::date;
+	Price_List := product_record->> 'PriceList';
 	product_categories := ARRAY(SELECT jsonb_array_elements_text(product_record->'Category'))::text[];
 	IF EXISTS (SELECT 1 FROM dic."Products" WHERE "ProductName" = product_name) THEN
 		RAISE NOTICE 'Product "%" already exists. skiped', product_name;
@@ -64,9 +66,9 @@ LOOP
 	VALUES (product_name, unit_id, product_ean)
 	RETURNING "ID" INTO return_product_id;
 	--- Вставляем Цены
-	RAISE NOTICE '%, %, %, %', return_product_id, product_price, price_start_date, price_end_date;
-	INSERT INTO dic."Price" ("ProductId", "Price", "StartDate", "EndDate")
-	VALUES (return_product_id, product_price, price_start_date, price_end_date);
+	RAISE NOTICE '%, %, %', return_product_id, product_price, Price_List;
+	INSERT INTO dic."Price" ("ProductId", "Price", "PriceListId")
+	VALUES (return_product_id, product_price, Price_List);
 	--- Начинаем вставлять категории
 	FOREACH product_category  IN ARRAY product_categories
 	LOOP
